7201c9f5712fa1f0a704ed5e3000ad8d9c8b72b5
Megan Matanda <97180810+Megan74@users.noreply.github.com>
2022-09-06 10:38:05 +0200

    Update Nav.js

--- components/_child/Nav.js
+++ components/_child/Nav.js
@@ -7,9 +7,9 @@ export default function Nav(){
    
     return (
 
-        <><nav className="class="navbar fixed-top navbar-expand-sm py-4 border-t border-b bg-gray-100" x-data="{ open: false }">
+        <><nav className="class=" mx-auto navbar fixed-top navbar-expand-sm py-4 border-t border-b bg-gray-100" x-data="{ open: false }">
     
-                <div className="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
+                <div className="container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
                     <Link href={'/article/post-1'}><a className="hover:bg-gray-400 rounded py-2 px-4 mx-2">Technology</a></Link>
                     <Link href={'/article/post-3'}><a className="hover:bg-gray-400 rounded py-2 px-4 mx-2">Fashion </a></Link>
                     <Link href={'/article/post-4'}><a  className="hover:bg-gray-400 rounded py-2 px-4 mx-2">Travel</a></Link>
