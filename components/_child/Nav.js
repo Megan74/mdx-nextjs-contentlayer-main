1a5479eb5e29d0d2efeef394f664362188886fe5
Megan Matanda <97180810+Megan74@users.noreply.github.com>
2022-09-06 10:34:07 +0200

    Update Nav.js

--- components/_child/Nav.js
+++ components/_child/Nav.js
@@ -7,7 +7,7 @@ export default function Nav(){
    
     return (
 
-        <><nav className="w-full py-4 border-t border-b bg-gray-100" x-data="{ open: false }">
+        <><nav className="class="navbar fixed-top navbar-expand-sm py-4 border-t border-b bg-gray-100" x-data="{ open: false }">
     
                 <div className="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
                     <Link href={'/article/post-1'}><a className="hover:bg-gray-400 rounded py-2 px-4 mx-2">Technology</a></Link>
