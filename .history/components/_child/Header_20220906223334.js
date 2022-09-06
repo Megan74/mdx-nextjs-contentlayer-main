d66fa2d13b71034ace70faba752652550107dc95
“Megan <mmegaan@gmail.com>
2022-09-06 22:34:22 +0200

    --all

--- .history/components/_child/Header_20220906223334.js
+++ .history/components/_child/Header_20220906223334.js
@@ -7,7 +7,7 @@ export default function Header() {
 
   return (
 
-    <><nav className=' md:h-2xl md:w-2xl w-full  m-auto  py-4 bg-blue-800 shadow'>
+    <><nav className=' md:h-full md:w-full w-full  m-auto  py-4 bg-blue-800 shadow'>
           <div className='w-full container mx-auto flex flex-wrap items-center justify-between'>
 
               <nav>
